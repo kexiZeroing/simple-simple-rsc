3f0386bca87a7e8c232a04efd5035793909789b2
Kexi Dang <dangkexi@gmail.com>
2024-06-09 09:55:37 +0800

    update

--- app/_client.jsx
+++ app/_client.jsx
@@ -1,6 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import { createFromFetch } from 'react-server-dom-webpack/client';
 
+// HACK: map webpack resolution to native ESM
+// window.__webpack_require__ = async (id) => {
+//   return import(id);
+// };
+
 const root = createRoot(document.getElementById('root'));
 
 /**
